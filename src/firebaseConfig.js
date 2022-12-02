91248766d5b0b50929298f4406c18e4de8c83f77
Botti13 <botti1391@hotmail.com>
2022-12-02 04:26:51 -0600

    auth casi casi

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBFm2mogBHPs-pcC0KX0Xb4a2KWwhEICYs",
+    authDomain: "vue-3-2022-3894a.firebaseapp.com",
+    projectId: "vue-3-2022-3894a",
+    storageBucket: "vue-3-2022-3894a.appspot.com",
+    messagingSenderId: "1011347355632",
+    appId: "1:1011347355632:web:d42cb9c55f16bd19200f35"
+};
+
+initializeApp(firebaseConfig);
+const auth = getAuth()
+
+export { auth }
