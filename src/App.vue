4c252ca279c19348cc1756921c7b90705b4acb83
Botti13 <botti1391@hotmail.com>
2022-12-08 03:21:31 -0600

    hosting listo

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div>
     <h1>App</h1>
     <nav v-if="!userStore.loadingSession">
-      <router-link to="/" v-if="userStore.userData">Home</router-link>
-      <router-link to="/login" v-if="!userStore.userData">Login</router-link>
-      <router-link to="/register" v-if="!userStore.userData">Register</router-link>
+      <router-link to="/" v-if="userStore.userData">Home</router-link> |
+      <router-link to="/login" v-if="!userStore.userData">Login</router-link> |
+      <router-link to="/register" v-if="!userStore.userData">Register</router-link> |
       <button @click="userStore.logOutUser" v-if="userStore.userData">Logout</button>
     </nav>
     <div v-else>
